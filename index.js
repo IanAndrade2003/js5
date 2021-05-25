fc24c28a4a432a1668d8108a20e72dba10133feb
IanAndrade2003 <ianandrade90@outlook.com>
2021-05-25 12:15:46 -0300

    Final

---
+++ index.js
@@ -0,0 +1,19 @@
+let anime = ["hajime no ippo","jujutsu kaisen","black clover","horimiya","boku no hero"];
+console.log(anime)
+
+anime.unshift("Golden time")
+console.log(anime)
+
+anime.pop()
+console.log(anime)
+
+anime.push("One piece","Mondaiji-tachi ga Isekai Kara Kuru So Desu yo?")
+console.log(anime)
+
+anime.shift()
+console.log(anime)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+console.log(numbers)
